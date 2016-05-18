e854fc605a2e1e36f618fe800ccfbeca7c9793b9
Debmalya Jash <debmalya.jash@gmail.com>
2016-05-19 07:05:01 +0800

    Adding MP3Recorder.java

---
+++ src/main/java/com/deb/vad/MP3Recorder.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.deb.vad;
+
+/**
+ * @author debmalyajash
+ *
+ */
+public class MP3Recorder {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
