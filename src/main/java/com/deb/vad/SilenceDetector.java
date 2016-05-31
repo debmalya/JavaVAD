0497c8d9e08c26cd0472c8aaa88a5926d0ad24e4
debmalyajash <debmalya.jash@gmail.com>
2016-05-31 18:50:22 +0800

    Trying to improve memory foot print

--- src/main/java/com/deb/vad/SilenceDetector.java
+++ src/main/java/com/deb/vad/SilenceDetector.java
@@ -50,7 +50,7 @@ public class SilenceDetector {
             line = (TargetDataLine)AudioSystem.getLine(info);
             line.open(format);
             line.start();
-            Arrays.fill(SILENCE, (byte)0);
+            
             AudioInputStream ais = new AudioInputStream(line);
             while (true) {
                 // An AudioInputStream is a subclass of the InputStream class,
@@ -59,18 +59,22 @@ public class SilenceDetector {
                 // adds knowledge of the bytes' audio data format (represented
                 // by an AudioFormat object).
 
+                int size = ais.available();
+                byte[] b = new byte[size];
+                SILENCE = new byte[size];
+                Arrays.fill(SILENCE, (byte)0);
+                ais.read(b, 0, size);
                 
-                byte[] b = new byte[ARRAY_SIZE];
-                ais.read(b, 0, ARRAY_SIZE);
                 if (Arrays.equals(SILENCE, b)) {
-//                    System.out.println("Slience :" + Arrays.toString(b));
+//                    System.out.println(size+ " slience :" + Arrays.toString(b));
                 } else {
-                    System.out.println("Sound :" + Arrays.toString(b));
+                    System.out.println(size+ " sound :" + Arrays.toString(b));
                 }
 
                 // The AudioSystem class provides methods for reading and
                 // writing sounds in different file formats, and for converting
                 // between different data formats.
+                
                 b = null;
             }
         } catch (Throwable e) {
