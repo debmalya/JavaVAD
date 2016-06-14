baabce8112f727c68c3c8881c4b7f290adb270bb
debmalyajash <debmalya.jash@gmail.com>
2016-06-14 09:59:23 +0800

    Introducint MapDB for voice activity detection

--- src/main/java/com/deb/vad/SilenceDetector.java
+++ src/main/java/com/deb/vad/SilenceDetector.java
@@ -14,6 +14,11 @@ import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.DataLine;
 import javax.sound.sampled.TargetDataLine;
 
+import org.mapdb.DB;
+import org.mapdb.DBMaker;
+import org.mapdb.HTreeMap;
+import org.mapdb.Serializer;
+
 import com.deb.vad.utility.CommonUtil;
 
 /**
@@ -21,80 +26,82 @@ import com.deb.vad.utility.CommonUtil;
  *
  */
 public class SilenceDetector {
-    /**
-     * 
-     */
-    private static final int ARRAY_SIZE = 8;
-    public static byte[] SILENCE = new byte[ARRAY_SIZE];
+	/**
+	 * 
+	 */
+	private static final int ARRAY_SIZE = 8;
+	public static byte[] SILENCE = new byte[ARRAY_SIZE];
 
-    public static void main(final String args[]) {
-        AudioFormat format = CommonUtil.getAudioFormat();
+	public static void main(final String args[]) {
+		AudioFormat format = CommonUtil.getAudioFormat();
 
-        // A line is an element of the digital audio "pipeline" that is, a path
-        // for moving audio into or out of the system. Usually the line is a
-        // path into or out of a mixer (although technically the mixer itself is
-        // also a kind of line).
+		// A line is an element of the digital audio "pipeline" that is, a path
+		// for moving audio into or out of the system. Usually the line is a
+		// path into or out of a mixer (although technically the mixer itself is
+		// also a kind of line).
 
-        // Audio input and output ports are lines. These are analogous to the
-        // microphones and speakers connected to a physical mixing console.
-        // Another kind of line is a data path through which an application
-        // program can get input audio from, or send output audio to, a mixer.
-        // These data paths are analogous to the tracks of the multitrack
-        // recorder connected to the physical mixing console.
-        DataLine.Info info = new DataLine.Info(TargetDataLine.class, format);
+		// Audio input and output ports are lines. These are analogous to the
+		// microphones and speakers connected to a physical mixing console.
+		// Another kind of line is a data path through which an application
+		// program can get input audio from, or send output audio to, a mixer.
+		// These data paths are analogous to the tracks of the multitrack
+		// recorder connected to the physical mixing console.
+		DataLine.Info info = new DataLine.Info(TargetDataLine.class, format);
 
-        // checks if system supports the data line
-        if (!AudioSystem.isLineSupported(info)) {
-            System.err.println("Line not supported. Sorry I am leaving...");
-            System.exit(0);
-        }
-        TargetDataLine line = null;
-        try {
-            line = (TargetDataLine)AudioSystem.getLine(info);
-            line.open(format);
-            line.start();
+		// checks if system supports the data line
+		if (!AudioSystem.isLineSupported(info)) {
+			System.err.println("Line not supported. Sorry I am leaving...");
+			System.exit(0);
+		}
+		TargetDataLine line = null;
+		try {
+			line = (TargetDataLine) AudioSystem.getLine(info);
+			line.open(format);
+			line.start();
 
-            AudioInputStream ais = new AudioInputStream(line);
-//            StringBuilder sb = new StringBuilder();
- 
-            Map<Long,byte[]> timeBasedAudioInputMap = new HashMap<>();
-            while (true) {
-                // An AudioInputStream is a subclass of the InputStream class,
-                // which encapsulates a series of bytes that can be read
-                // sequentially. To its superclass, the AudioInputStream class
-                // adds knowledge of the bytes' audio data format (represented
-                // by an AudioFormat object).
+			AudioInputStream ais = new AudioInputStream(line);
+			// StringBuilder sb = new StringBuilder();
+			
+			Map<Long,byte[]> timeBasedAudioInputMap = new HashMap<>();
 
-                int size = ais.available();
-                byte[] b = new byte[size];
-                SILENCE = new byte[size];
-                Arrays.fill(SILENCE, (byte)0);
-                ais.read(b, 0, size);
+			
+			while (true) {
+				// An AudioInputStream is a subclass of the InputStream class,
+				// which encapsulates a series of bytes that can be read
+				// sequentially. To its superclass, the AudioInputStream class
+				// adds knowledge of the bytes' audio data format (represented
+				// by an AudioFormat object).
 
-                if (Arrays.equals(SILENCE, b)) {                  
-                } else {
-                	timeBasedAudioInputMap.put(System.currentTimeMillis(),b);
-                  
-                }
+				int size = ais.available();
+				byte[] b = new byte[size];
+				SILENCE = new byte[size];
+				Arrays.fill(SILENCE, (byte) 0);
+				ais.read(b, 0, size);
 
-                // The AudioSystem class provides methods for reading and
-                // writing sounds in different file formats, and for converting
-                // between different data formats.
+				if (Arrays.equals(SILENCE, b)) {
+				} else {
+					timeBasedAudioInputMap.put(System.currentTimeMillis(), b);
 
-//                b = null;
-                System.out.println("Alive...");
-            }
-        } catch (Throwable e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        } finally {
-            if (line != null) {
-                line.flush();
-                line.close();
-            }
-        }
+				}
 
-    }
+				// The AudioSystem class provides methods for reading and
+				// writing sounds in different file formats, and for converting
+				// between different data formats.
+
+				// b = null;
+				System.out.println("Alive...");
+			}
+		} catch (Throwable e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			if (line != null) {
+				line.flush();
+				line.close();
+			}
+		}
+
+	}
 
 	private static void getInputRange(byte[] b) {
 		int total;
@@ -102,14 +109,14 @@ public class SilenceDetector {
 		byte min = Byte.MAX_VALUE;
 		total = 0;
 		for (byte each : b) {
-		    if (each < min) {
-		        min = each;
-		    } else if (each > max){
-		        max = each;
-		    }
-		    total += each;
+			if (each < min) {
+				min = each;
+			} else if (each > max) {
+				max = each;
+			}
+			total += each;
 		}
-		System.out.println(new Date() + " "+total+ " Range " + min + " ~ " + max);
+		System.out.println(new Date() + " " + total + " Range " + min + " ~ " + max);
 	}
 
 }
---
+++ src/main/java/com/deb/vad/SilenceDetectorWithHTreeMap.java
@@ -0,0 +1,121 @@
+package com.deb.vad;
+
+import java.util.Arrays;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+/**
+ * 
+ */
+import javax.sound.sampled.DataLine;
+import javax.sound.sampled.TargetDataLine;
+
+import org.mapdb.DB;
+import org.mapdb.DBMaker;
+import org.mapdb.HTreeMap;
+import org.mapdb.Serializer;
+
+import com.deb.vad.utility.CommonUtil;
+
+/**
+ * @author debmalyajash
+ *
+ */
+public class SilenceDetectorWithHTreeMap {
+    /**
+     * 
+     */
+    private static final int ARRAY_SIZE = 8;
+    public static byte[] SILENCE = new byte[ARRAY_SIZE];
+
+    public static void main(final String args[]) {
+        AudioFormat format = CommonUtil.getAudioFormat();
+
+        // A line is an element of the digital audio "pipeline" that is, a path
+        // for moving audio into or out of the system. Usually the line is a
+        // path into or out of a mixer (although technically the mixer itself is
+        // also a kind of line).
+
+        // Audio input and output ports are lines. These are analogous to the
+        // microphones and speakers connected to a physical mixing console.
+        // Another kind of line is a data path through which an application
+        // program can get input audio from, or send output audio to, a mixer.
+        // These data paths are analogous to the tracks of the multitrack
+        // recorder connected to the physical mixing console.
+        DataLine.Info info = new DataLine.Info(TargetDataLine.class, format);
+
+        // checks if system supports the data line
+        if (!AudioSystem.isLineSupported(info)) {
+            System.err.println("Line not supported. Sorry I am leaving...");
+            System.exit(0);
+        }
+        TargetDataLine line = null;
+        try {
+            line = (TargetDataLine)AudioSystem.getLine(info);
+            line.open(format);
+            line.start();
+
+            AudioInputStream ais = new AudioInputStream(line);
+//            StringBuilder sb = new StringBuilder();
+ 
+            DB db = DBMaker.memoryDB().make();
+			HTreeMap<Long, byte[]> timeBasedAudioInputMap = db.hashMap("long_byte_array_map", Serializer.LONG, Serializer.BYTE_ARRAY).create();
+            while (true) {
+                // An AudioInputStream is a subclass of the InputStream class,
+                // which encapsulates a series of bytes that can be read
+                // sequentially. To its superclass, the AudioInputStream class
+                // adds knowledge of the bytes' audio data format (represented
+                // by an AudioFormat object).
+
+                int size = ais.available();
+                byte[] b = new byte[size];
+                SILENCE = new byte[size];
+                Arrays.fill(SILENCE, (byte)0);
+                ais.read(b, 0, size);
+
+                if (Arrays.equals(SILENCE, b)) {                  
+                } else {
+                	timeBasedAudioInputMap.put(System.currentTimeMillis(),b);
+                  
+                }
+
+                // The AudioSystem class provides methods for reading and
+                // writing sounds in different file formats, and for converting
+                // between different data formats.
+
+//                b = null;
+                System.out.println("Alive...");
+            }
+        } catch (Throwable e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } finally {
+            if (line != null) {
+                line.flush();
+                line.close();
+            }
+        }
+
+    }
+
+	private static void getInputRange(byte[] b) {
+		int total;
+		byte max = Byte.MIN_VALUE;
+		byte min = Byte.MAX_VALUE;
+		total = 0;
+		for (byte each : b) {
+		    if (each < min) {
+		        min = each;
+		    } else if (each > max){
+		        max = each;
+		    }
+		    total += each;
+		}
+		System.out.println(new Date() + " "+total+ " Range " + min + " ~ " + max);
+	}
+
+}
